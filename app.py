d8c780c73bf8aaaa84e8dcb13d59c975b4df0308
brianna-341 <158204317+brianna-341@users.noreply.github.com>
2024-05-07 12:28:43 -0400

    sdf

--- app.py
+++ app.py
@@ -1,28 +1,50 @@
 
+class dialouge:
+    def __init__(self,dStart,dA,dnpc,dP,dB,dEnd):
+        dStart = dStart
+        self.dA = dA
+        self.dnpc = dnpc
+        self.dP = dP
+        self.dB = dB
+        self.dEnd = dEnd
 
+        pass
 
 
 class animal:
-    def __init__(self,type,health):
+    def __init__(self,type,hp,attack):
+        self.type = type
+        self.hp = hp
+        self.attack = attack
 
         pass
 
 class npc:
-    def __init__(self):
+    def __init__(self,name):
+        self.name = name
+        self.dialouge = dialouge
 
         pass
 
 class player:
-    def __init__(self):
+    def __init__(self,name1,hp1,attack1):
+        self.name1 = name1
+        self.hp1 = hp1
+        self.attack1 = attack1
 
         pass
 
 class Boss:
-    def __init__(self):
+    def __init__(self,name2,hp2,attack2):
+        self.name2 = name2
+        self.hp2 = hp2
+        self.attack2 = attack2
 
         pass
 
-class items:
-    def __init__(self):
-
+class Items:
+    def __init__(self,title,heal,boost):
+        self.title = title
+        self.heal = heal
+        self.boost = boost
         pass
