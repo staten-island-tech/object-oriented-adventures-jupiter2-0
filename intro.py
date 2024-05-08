20b1dfc0051ddc7795da4080312a07f499e2cd43
vicky-2009 <vickyo4@nycstudents.net>
2024-05-08 12:28:18 -0400

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,9 @@
+print("Welcome to __. This is the begining of your journey. Catch and level up to defeat bosses. The final goal of your journey is to defeat the final boss.")
+name = input("Choose a name for your charcter:")
+player = name
+
+
+
+
+    
+    
