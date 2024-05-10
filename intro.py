6498029479f1a3bd7b301fcb8bf4e42bcfdbfa4f
vicky-2009 <vickyo4@nycstudents.net>
2024-05-10 12:29:48 -0400

    Update intro.py

--- intro.py
+++ intro.py
@@ -10,7 +10,19 @@ while True:
 
 print("These are you're starter pets.")
 
+class character:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
+def move (self, direction):
+    if direction == "up":
+        self.y += 1
+    elif direction == "down":
+        self.y -= 1
+    elif direction == "left":
+        self.x -= 1
+        
 
 
     
