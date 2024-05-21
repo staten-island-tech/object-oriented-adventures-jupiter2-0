30c7454f4f152b5577659bccc68ad9afe7cc04d1
brianna-341 <158204317+brianna-341@users.noreply.github.com>
2024-05-21 12:19:35 -0400

    asd

--- dialouge.py
+++ dialouge.py
@@ -1,16 +1,17 @@
 
 def dnpc(self):
-    def Nurse():
-        Nurse = input("Nurse:Would you like to heal your teammate? (Y/N)")
+    def Nurse1():
+        Nurse = input("Nurse: Would you like to heal your teammate? (Y/N)")
         if Nurse == "Y":
-            ("Nurse:Your companion has healed")
-    def Merchant():
+            print("Nurse: Your companion has healed!",)
+    def Merchant1():
         Merchant = input("Merchant:Would you like to buy items? (Y/N)")
         if Merchant == "Y":
             ("Items:")
-    def Npc8():
-        Npc8 = ("Hello! Meet with others and save us from the torment of the Boss.")
-            #("Your companion has healed {hpup}")
+    def Violet1():
+        Violet = print("Violet: Hello! Meet with others and save us from the torment of the Boss.")
+
+        
 def dB(self):
     if health == 100:
         print("What are you doing here!!")
